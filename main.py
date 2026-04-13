cfd03f412aa3c584d5a4f988e9c063538830bba5
Sourabh Issac <s.steve.issac@gmail.com>
2026-04-13 08:24:17 +0100

    Small addition of .gitignore, venv and requirements.txt (after installing numpy and some other bits)

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Board:
+    def __init__(self, grid):
+        # Holds the attributes for the methods used within the class definition
+        grid = np.zeros((8, 8))
+
+        self.grid = grid
+
+
+    def display(self, grid):
+        # Prints out grid in string form (would be acted upon by move generation and such later)
+        for item in
+        
+    def get_square(self):
+        # Returns the string for the element of the grid array
+        pass
